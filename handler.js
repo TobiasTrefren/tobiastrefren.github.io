f393a5e6398a99ed86b93d4de00be0d3bad19ede
Craze-Dev <crazetechnologiesinc@gmail.com>
2025-01-21 18:55:55 -0600

    Added Extra Features
    Added more hover descriptions
    Added about me section
    Added Key Projects Section

--- handler.js
+++ handler.js
@@ -1,9 +1,22 @@
 const school = document.getElementById('school')
+const degree = document.getElementById('degree')
 
 school.addEventListener('mouseover', () =>{
     school.innerHTML = "Western Governors University";
+    school.style.fontSize = "18px";
 });
 
 school.addEventListener('mouseout', () =>{
     school.innerHTML = "School: WGU";
+    school.style.fontSize = "24px";
+});
+
+degree.addEventListener('mouseover', () =>{
+    degree.innerHTML = "Bachelors in Computer Science";
+    degree.style.fontSize = "18px";
+});
+
+degree.addEventListener('mouseout', () =>{
+    degree.innerHTML = "Degree: BSCS";
+    degree.style.fontSize = "24px";
 });
